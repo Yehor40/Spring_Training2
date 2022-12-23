2e987990b3f75dc545170c9985f312a7389a4a98
Yehor40 <fraugrey3@gmail.com>
2022-12-23 15:14:21 +0100

    Advanced autowired options

--- src/main/java/com/example/demo/config/HelloConfig.java
+++ src/main/java/com/example/demo/config/HelloConfig.java
@@ -6,6 +6,7 @@ import com.example.demo.services.HelloWorldServiceEng;
 import com.example.demo.services.HelloWorldfactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
@@ -14,16 +15,26 @@ public class HelloConfig {
    public HelloWorldfactory helloWorldfactory(){
        return new HelloWorldfactory();
    }
-
-
     @Bean
     @Profile("english")
+    @Primary
     public HelloWorldService hlse(HelloWorldfactory factory){
         return  factory.createHelloService("en");
     }
     @Bean
     @Profile("czech")
+    @Primary
     public HelloWorldService hlsc(HelloWorldfactory factory){
         return  factory.createHelloService("cze");
+    }
+    @Bean(name="german")
+    //@Profile("german")
+    public HelloWorldService hlsd(HelloWorldfactory factory){
+        return  factory.createHelloService("de");
+    }
+    @Bean
+    //@Profile("ukraine")
+    public HelloWorldService hlsu(HelloWorldfactory factory){
+        return  factory.createHelloService("ukr");
     }
 }
--- src/main/java/com/example/demo/controllers/Greeting.java
+++ src/main/java/com/example/demo/controllers/Greeting.java
@@ -1,20 +1,28 @@
 package com.example.demo.controllers;
 
 import com.example.demo.services.HelloWorldService;
+import com.example.demo.services.HelloWorldServiceDe;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class Greeting {
     private HelloWorldService hls;
-
+    private HelloWorldService hlsde;
     @Autowired
     public void setHls(HelloWorldService hls) {
         this.hls = hls;
     }
+    @Autowired
+    @Qualifier("german")
+    public void setHlsd(HelloWorldService hlsd) {
+        this.hlsde = hlsd;
+    }
 
     public String sayHello(){
         String greet = hls.GetGreeting();
+        System.out.println(hlsde.GetGreeting());
         System.out.println(greet);
         return greet;
     }
